29a95b7dfaf77c332193c99d880ecb5f84f0023c
Tomasz Szczepański <tszczepanski@suncode.pl>
2025-05-10 14:23:36 +0200

    fix: astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  server: { port: 3000 },
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: import.meta.env.PROD
         ? {
